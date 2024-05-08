389f7cf01da4b509a85505ca35b4704c2a8d961b
Prathmesh <prathmesh.lathkar1@gmail.com>
2024-05-08 14:43:00 +0530

    Create sessionbyospid.sql

---
+++ Session/sessionbyospid.sql
@@ -0,0 +1,18 @@
+--
+-- List Session ID for a Given OS Process ID.
+--
+ 
+SET PAUSE ON
+SET PAUSE 'Press Return to Continue'
+SET PAGESIZE 60
+SET LINESIZE 300
+ 
+SELECT
+   CHR(10)||
+   'Check for SESSION ID ---->  '||LPAD( s.sid, 4 )||CHR(10)||CHR(10) as "Session ID"
+FROM
+   v$session s, v$process p
+WHERE
+   p.addr = s.paddr AND
+   p.spid = &Enter_OSPid
+/
